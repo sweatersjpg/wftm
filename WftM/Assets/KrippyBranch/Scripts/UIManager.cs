703e0ea0244ff5e70e6907e751d674938e005d5a
KripKode <55769242+KripKode@users.noreply.github.com>
2023-03-17 12:38:39 -0400

    Fixed Menu & Pause Menu

--- WftM/Assets/KrippyBranch/Scripts/UIManager.cs
+++ WftM/Assets/KrippyBranch/Scripts/UIManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
@@ -7,147 +5,88 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField]
-    bool isMainMenu;
+    [SerializeField] GameObject optionsPanel;
+    [SerializeField] Slider sliderMusic, sliderSFX;
+    [SerializeField] AudioMixer musicMixer, sfxMixer;
 
-    [SerializeField]
-    GameObject pausePanel, mainPanel, optionsPanel;
-
-    public static bool isGamePaused;
-    bool doneCheck;
-
-    //Audio
-    public AudioMixer musicMixer, sfxMixer;
-    string volumeParameter = "Volume";
-
-    public Slider sliderMusic, sliderSFX;
-
+    public bool canBePaused = false;
+    static bool isGamePaused = false;
     bool doOnce;
 
-    // Update is called once per frame
+    string volumeParameter = "Volume";
+
+    void Awake()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        DontDestroyOnLoad(gameObject);
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 0)
+            canBePaused = false;
+        else
+            canBePaused = true;
+    }
+
     void Update()
     {
-        checkVolume();
-        CheckPausedState();
-
-        if (isMainMenu)
-        {
-            isGamePaused = true; //just done so menu is shown, same system as Pause
-            return;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
+        if (Input.GetKeyDown(KeyCode.Escape) && canBePaused)
             isGamePaused = !isGamePaused;
-        }
-    }
 
-    void checkVolume()
-    {
         if (!doOnce)
-        {
-            // Music
-            float Mvolume = PlayerPrefs.GetFloat("MVolume");
+            LoadVolume();
 
-            if (Mvolume == 0)
-            {
-                musicMixer.SetFloat(volumeParameter, -80);
-            }
-            else
-            {
-                musicMixer.SetFloat(volumeParameter, Mathf.Log10(Mvolume) * 20);
-            }
-
-            sliderMusic.value = Mvolume;
-            sliderMusic.onValueChanged.AddListener(delegate { musicSlider(sliderMusic.value); });
-
-            // SFX
-            float SFXvolume = PlayerPrefs.GetFloat("SFXVolume");
-
-            if (SFXvolume == 0)
-            {
-                sfxMixer.SetFloat(volumeParameter, -80);
-            }
-            else
-            {
-                sfxMixer.SetFloat(volumeParameter, Mathf.Log10(SFXvolume) * 20);
-            }
-
-            sliderSFX.value = SFXvolume;
-            sliderSFX.onValueChanged.AddListener(delegate { sfxSlider(sliderSFX.value); });
-            doOnce = true;
-        }
+        CheckPausedState();
     }
+
+    void LoadVolume()
+    {
+        float Mvolume = PlayerPrefs.GetFloat("MVolume", 0);
+        float SFXvolume = PlayerPrefs.GetFloat("SFXVolume", 0);
+
+        musicMixer.SetFloat(volumeParameter, Mvolume == 0 ? -80 : Mathf.Log10(Mvolume) * 20);
+        sliderMusic.value = Mvolume;
+        sliderMusic.onValueChanged.AddListener(delegate { musicMixer.SetFloat(volumeParameter, Mathf.Log10(sliderMusic.value) * 20); PlayerPrefs.SetFloat("MVolume", sliderMusic.value); });
+
+        sfxMixer.SetFloat(volumeParameter, SFXvolume == 0 ? -80 : Mathf.Log10(SFXvolume) * 20);
+        sliderSFX.value = SFXvolume;
+        sliderSFX.onValueChanged.AddListener(delegate { sfxMixer.SetFloat(volumeParameter, Mathf.Log10(sliderSFX.value) * 20); PlayerPrefs.SetFloat("SFXVolume", sliderSFX.value); });
+
+        doOnce = true;
+    }
+
     void CheckPausedState()
     {
-        switch (isGamePaused)
+        if (isGamePaused)
         {
-            case true:
-                if (doneCheck)
-                    return;
-                if(!isMainMenu)
-                {
-                    AudioListener.pause = true;
-                    Time.timeScale = 0f;
-                }
-                mainPanel.SetActive(true);
-                pausePanel.SetActive(true);
-                doneCheck = true;
-                break;
-            case false:
-                AudioListener.pause = false;
-                optionsPanel.SetActive(false);
-                pausePanel.SetActive(false);
-                Time.timeScale = 1f;
-                doneCheck = false;
-                break;
+            optionsPanel.SetActive(true);
+            AudioListener.pause = true;
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            AudioListener.pause = false;
+            optionsPanel.SetActive(false);
+            Time.timeScale = 1f;
         }
     }
 
     public void Resume()
     {
         isGamePaused = false;
-    }
-
-    public void GoToMenu(int levelIndex)
-    {
-        isGamePaused = false;
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(levelIndex);
+        CheckPausedState();
     }
 
     public void musicSlider(float value)
     {
-        if (value == 0)
-        {
-            musicMixer.SetFloat(volumeParameter, -80);
-            PlayerPrefs.SetFloat("MVolume", 0);
-        }
-        else
-        {
-            musicMixer.SetFloat(volumeParameter, Mathf.Log10(value) * 20);
-            PlayerPrefs.SetFloat("MVolume", value);
-        }
+        musicMixer.SetFloat(volumeParameter, value == 0 ? -80 : Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("MVolume", value);
     }
 
     public void sfxSlider(float value)
     {
-        if (value == 0)
-        {
-            sfxMixer.SetFloat(volumeParameter, -80);
-            PlayerPrefs.SetFloat("SFXVolume", 0);
-        }
-        else
-        {
-            sfxMixer.SetFloat(volumeParameter, Mathf.Log10(value) * 20);
-            PlayerPrefs.SetFloat("SFXVolume", value);
-        }
-    }
-
-    private void OnApplicationQuit()
-    {
-        PlayerPrefs.SetFloat("MVolume", sliderMusic.value);
-        PlayerPrefs.SetFloat("SFXVolume", sliderSFX.value);
+        sfxMixer.SetFloat(volumeParameter, value == 0 ? -80 : Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", value);
     }
 
     public void StartGame(int levelIndex)
@@ -156,8 +95,5 @@ public class UIManager : MonoBehaviour
         SceneManager.LoadScene(levelIndex);
     }
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+    public void QuitGame() => Application.Quit();
 }
